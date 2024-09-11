e312b56632957ad97655109fb2f2f25c8a837deb
Mariana Picolo <26073710+MarianaPicolo@users.noreply.github.com>
2024-09-11 18:57:18 -0300

    fix: container responsiveness

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,3 +1,9 @@
 export function Container({ children, maxWidth }: { children: React.ReactNode; maxWidth?: number }) {
-  return <div className={`max-w-[${maxWidth ? maxWidth : '--global-max-width'}] h-full`}>{children}</div>;
+  return (
+    <div className={'w-full h-full flex justify-center '}>
+      <div className={`w-full ${maxWidth ? `max-w-[${maxWidth}px]` : 'max-w-[--global-max-width]'}`}>
+        {children}
+      </div>
+    </div>
+  );
 }
