026e005ea0cf4ccfd3b11683a21941d0ed52d036
Keegan Francis <Keegan-lee@users.noreply.github.com>
2024-09-19 11:19:29 -0400

    feat: implement launch collection view in #28 (#30)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from '@/components/Container';
+import { Button } from '@/components/ui/button';
 import { DEFAULT_METADATA } from '@/lib/constants';
 import { Metadata } from 'next';
 
@@ -16,6 +17,31 @@ export default function Home() {
             </h1>
           </div>
         </Container>
+      </div>
+
+      <div className='flex h-full max-h-[100vh] w-full flex-row items-center justify-center overflow-hidden bg-ob-black-lighter'>
+        <div className='flex h-full w-1/2 flex-col justify-start gap-8 p-24 text-white'>
+          <h2 className='bold text-6xl capitalize'>Launch a Collection</h2>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam.
+          </span>
+          <Button variant='secondary'>Launch</Button>
+        </div>
+
+        <div className='flex w-1/2 flex-row items-center justify-center gap-4'>
+          <div className='-mb-96 flex h-full w-2/5 flex-col gap-4'>
+            {Array.from({ length: 4 }).map((_, index) => {
+              return <div key={index} className='h-[420px] max-h-[420px] w-full rounded-xl bg-[#252525]'></div>;
+            })}
+          </div>
+
+          <div className='mb-16 flex h-full w-2/5 flex-col gap-4'>
+            {Array.from({ length: 4 }).map((_, index) => {
+              return <div key={index} className='h-[420px] max-h-[420px] w-full rounded-xl bg-[#252525]'></div>;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
