bbdad1caec414125a93ba3b18c0af8f0f1be8a46
peter514n <peter514n@tutamail.com>
2024-12-04 15:20:23 +0100

    fix: revert splashpage layout change

--- src/components/Layouts/SplashPageLayout.tsx
+++ src/components/Layouts/SplashPageLayout.tsx
@@ -1,40 +1,43 @@
-import { useMemo } from 'react';
+import { ReactNode } from 'react';
 import { MediaWrapper } from '../common';
 import Image from 'next/image';
-import { EJustify, EOrientation, TSplashPageLayoutProps } from '@/types';
+
+interface SplashPageLayoutProps {
+  children: ReactNode;
+  orientation?: 'ltr' | 'rtl';
+  media: {
+    type: 'inscription' | 'img';
+    id?: string;
+    src?: string;
+  };
+  className?: string;
+  childrenWrapperJustify?: 'start' | 'center' | 'end' | 'between' | 'around' | 'evenly';
+}
 
 export default function SplashPageLayout({
   children,
-  orientation = EOrientation.LTR,
+  orientation = 'ltr',
   media: { type, id, src },
   className,
-  childrenWrapperJustify = EJustify.CENTER
-}: TSplashPageLayoutProps) {
-  const ImgSkeleton = () => <div className='h-full w-full animate-pulse rounded-xl bg-ob-black-lightest'></div>;
-
-  const splashImage = useMemo(() => {
-    if (!src) return <ImgSkeleton />;
-    if (type === 'inscription') return <ImgSkeleton />;
-    if (type === 'img')
-      return (
-        <Image
-          src={src ?? ''}
-          alt='some hero image'
-          width={1000}
-          height={1000}
-          className='relative overflow-hidden rounded-xl'
-        />
-      );
-    return (
-      <div className='w-full'>
-        <MediaWrapper id={id} size='full' className='relative max-w-[600px] overflow-hidden rounded-xl' />
-      </div>
-    );
-  }, [src, type]);
-
+  childrenWrapperJustify = 'center'
+}: SplashPageLayoutProps) {
   return (
     <div className={`flex flex-row ${orientation === 'rtl' ? 'flex-row-reverse' : ''} ${className ? className : ''}`}>
-      <div className='flex w-1/2 basis-1/2 items-start justify-start'>{splashImage}</div>
+      <div className='flex basis-1/2 items-start justify-start'>
+        {type === 'inscription' ? (
+          <div className='w-full'>
+            <MediaWrapper id={id} size='full' className='relative max-w-[600px] overflow-hidden rounded-xl' />
+          </div>
+        ) : (
+          <Image
+            src={src ?? ''}
+            alt='some hero image'
+            width={1000}
+            height={1000}
+            className='relative overflow-hidden rounded-xl'
+          />
+        )}
+      </div>
       <div className={`flex basis-1/2 flex-col justify-${childrenWrapperJustify}`}>{children}</div>
     </div>
   );
