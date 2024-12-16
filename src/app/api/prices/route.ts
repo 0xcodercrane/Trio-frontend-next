41592073b889d8d1b1397397c100647f979fedce
peter514n <peter514n@tutamail.com>
2024-12-16 16:21:23 +0100

    fix: disable nextjs cache (#267)

--- src/app/api/prices/route.ts
+++ src/app/api/prices/route.ts
@@ -1,28 +1,22 @@
 import { NextResponse } from 'next/server';
-import { unstable_cache } from 'next/cache';
 import { FxRateResponse } from '@/types';
 import { PUBLIC_API_URL } from '@/lib/constants';
 
-const CACHING_PERIOD_SECONDS = 600;
-const fetchBitcoinPrice = unstable_cache(
-  async () => {
-    try {
-      const prices: FxRateResponse = await (await fetch(`${PUBLIC_API_URL}/fxrate`)).json();
-      return prices;
-    } catch (e) {
-      throw new Error('Fetching BTC price from OrdinalsBot API failed.');
-    }
-  },
-  ['prices', 'btc'],
-  { revalidate: CACHING_PERIOD_SECONDS }
-);
+const fetchBitcoinPrice = async () => {
+  try {
+    const prices: FxRateResponse = await (await fetch(`${PUBLIC_API_URL}/fxrate`)).json();
+    return prices;
+  } catch (e) {
+    throw new Error('Fetching BTC price from OrdinalsBot API failed.');
+  }
+};
 
 export async function GET() {
   try {
     const prices = await fetchBitcoinPrice();
 
     const response = NextResponse.json(prices);
-    response.headers.set('Cache-Control', `s-maxage=${CACHING_PERIOD_SECONDS}, stale-while-revalidate`);
+    response.headers.set('Cache-Control', 'no-store');
 
     return response;
 
--- src/app/api/supabase/feeRates/route.ts
+++ src/app/api/supabase/feeRates/route.ts
@@ -1,16 +1,11 @@
 import { NextResponse } from 'next/server';
 import createClient from '..';
-import { unstable_cache } from 'next/cache';
 import { SupabaseClient } from '@supabase/supabase-js';
 import { Database } from '@/types';
-import { FEE_RATES_CACHE_AGE } from '@/lib/constants';
 
-// MEMO: Cache result on server for 30 seconds before firing out next db query
 let supabaseClient: SupabaseClient<Database>;
 
-const getFeeRates = unstable_cache(async () => supabaseClient.rpc('get_latest_fee_rate'), ['fee-rates'], {
-  revalidate: FEE_RATES_CACHE_AGE.as('seconds')
-});
+const getFeeRates = async () => supabaseClient.rpc('get_latest_fee_rate');
 
 export async function GET() {
   try {
@@ -18,7 +13,7 @@ export async function GET() {
     const feeRates = await getFeeRates();
 
     const response = NextResponse.json(feeRates);
-    response.headers.set('Cache-Control', `s-maxage=${FEE_RATES_CACHE_AGE.as('seconds')}, stale-while-revalidate`);
+    response.headers.set('Cache-Control', 'no-store');
     return response;
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
