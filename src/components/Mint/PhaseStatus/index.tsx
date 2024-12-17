5d6a81923da76ca6ce4c6cc31e7e7c46c8c181f7
David Dacruz <dvddcrz@gmail.com>
2024-12-17 16:56:19 +0000

    Fix/mint page start end UI indicator (#290)
    * Fix mint status display ui indicator
    
    * Fix mint status display ui indicator
    
    ---------
    
    Co-authored-by: Keegan-Lee <keegan.lee.francis@gmail.com>

--- src/components/Mint/PhaseStatus/index.tsx
+++ src/components/Mint/PhaseStatus/index.tsx
@@ -33,13 +33,18 @@ const mapStateToColor = (state: EPhaseState) => {
 
 const mapPhaseToState = (phase: TPhase): EPhaseState => {
   const now = Date.now();
-  if (now > phase.end_date * 1000) {
+  const start = phase.start_date * 1000;
+  const end = phase.end_date ? phase.end_date * 1000 : null;
+
+  if (end && now > end) {
     return EPhaseState.ENDED;
-  } else if (now >= phase.start_date * 1000 && now <= phase.end_date * 1000) {
-    return EPhaseState.LIVE;
-  } else {
-    return EPhaseState.UPCOMING;
   }
+
+  if (now >= start && (!end || now <= end)) {
+    return EPhaseState.LIVE;
+  }
+
+  return EPhaseState.UPCOMING;
 };
 
 const PhaseStatus: React.FC<TPhaseStatusProps> = ({
@@ -61,69 +66,73 @@ const PhaseStatus: React.FC<TPhaseStatusProps> = ({
 
   return (
     <div className='flex flex-col gap-3'>
-      {launchInfo.phases.map((phase: TPhase, index: number) => {
-        const { price } = phase;
+      {launchInfo.phases
+        .sort((a, b) => a.start_date - b.start_date)
+        .map((phase: TPhase, index: number) => {
+          const { price } = phase;
 
-        const btcPrice = satsToBitcoin(price);
-        const state = mapPhaseToState(phase);
-        const color = mapStateToColor(state);
+          const btcPrice = satsToBitcoin(price);
+          const state = mapPhaseToState(phase);
+          const color = mapStateToColor(state);
 
-        const allocationForPhase: TAllocation | null =
-          (!allocationIsPending && allocationInfo?.phases.find((allocation: TAllocation) => allocation.id === phase.id)) ||
-          null;
+          const allocationForPhase: TAllocation | null =
+            (!allocationIsPending && allocationInfo?.phases.find((allocation: TAllocation) => allocation.id === phase.id)) ||
+            null;
 
-        let inscriptionsRemaining = -1;
+          let inscriptionsRemaining = -1;
 
-        if (!phase.is_public) {
-          inscriptionsRemaining = allocationForPhase ? allocationForPhase.remaining_allocation : 0;
-        }
+          if (!phase.is_public) {
+            inscriptionsRemaining = allocationForPhase ? allocationForPhase.remaining_allocation : 0;
+          }
 
-        return (
-          <div
-            key={phase.id}
-            className={`relative flex h-24 max-h-24 flex-row justify-between rounded-xl p-[1px] ${
-              phase.id !== currentPhase?.id ? 'opacity-60 hover:cursor-not-allowed' : ''
-            }`}
-          >
+          return (
             <div
-              className={`absolute inset-0 h-full w-full rounded-xl ${
-                state === EPhaseState.LIVE ? 'bg-gradient-to-r from-ob-blue to-ob-green' : 'bg-ob-white-20'
+              key={phase.id}
+              className={`relative flex h-24 max-h-24 flex-row justify-between rounded-xl p-[1px] ${
+                phase.id !== currentPhase?.id ? 'opacity-60 hover:cursor-not-allowed' : ''
               }`}
-            ></div>
-            <div className='relative flex h-full min-h-[calc(var(--button-height-sm)-2px)] w-full max-w-full flex-row items-center justify-between gap-2 rounded-xl bg-ob-black-lightest px-4 xs:text-sm md:text-base'>
-              <div className='max-w-1/2 flex flex-col gap-2'>
-                <div className='flex flex-row justify-start gap-2'>
-                  <span className='text-lg'>{phase.name || `Phase ${index + 1}`}</span>
-                </div>
-                <span className='text-sm'>
-                  {btcPrice} BTC ({numeral(price).format('0a')} Sats)
-                </span>
-              </div>
-
-              <div className='max-w-1/2 flex max-h-full flex-col gap-2'>
-                <div className='flex flex-row justify-between gap-2'>
-                  <span className={`text-${color}`}>
-                    {state} {state === EPhaseState.LIVE && <span className='px-2 text-white'>Ends in</span>}
+            >
+              <div
+                className={`absolute inset-0 h-full w-full rounded-xl ${
+                  state === EPhaseState.LIVE ? 'bg-gradient-to-r from-ob-blue to-ob-green' : 'bg-ob-white-20'
+                }`}
+              ></div>
+              <div className='relative flex h-full min-h-[calc(var(--button-height-sm)-2px)] w-full max-w-full flex-row items-center justify-between gap-2 rounded-xl bg-ob-black-lightest px-4 xs:text-sm md:text-base'>
+                <div className='max-w-1/2 flex flex-col gap-2'>
+                  <div className='flex flex-row justify-start gap-2'>
+                    <span className='text-lg'>{phase.name || `Phase ${index + 1}`}</span>
+                  </div>
+                  <span className='text-sm'>
+                    {btcPrice} BTC ({numeral(price).format('0a')} Sats)
                   </span>
-                  <TimeLeft targetTime={state === EPhaseState.LIVE ? phase.end_date : phase.start_date} />
                 </div>
-                <div className='flex w-full items-center justify-end gap-1 text-right'>
-                  {isAuthenticated && (
-                    <span
-                      className={`block text-right ${state === 'Ended' && inscriptionsRemaining > 0 ? 'line-through' : ''}`}
-                    >
-                      {mapInscriptionsRemainingToContent(inscriptionsRemaining)}
+
+                <div className='max-w-1/2 flex max-h-full flex-col gap-2'>
+                  <div className='flex flex-row justify-between gap-2'>
+                    <span className={`text-${color}`}>
+                      {state} {state === EPhaseState.LIVE && <span className='px-2 text-white'>Ends in</span>}
                     </span>
-                  )}
-                  {allocationForPhase && !allocationForPhase.is_public ? (
-                    <Image src='/img/lock.svg' alt='discord' width={15} height={15} />
-                  ) : null}
+                    {state !== EPhaseState.ENDED && (
+                      <TimeLeft targetTime={state === EPhaseState.LIVE ? phase.end_date : phase.start_date} />
+                    )}
+                  </div>
+                  <div className='flex w-full items-center justify-end gap-1 text-right'>
+                    {isAuthenticated && (
+                      <span
+                        className={`block text-right ${state === 'Ended' && inscriptionsRemaining > 0 ? 'line-through' : ''}`}
+                      >
+                        {mapInscriptionsRemainingToContent(inscriptionsRemaining)}
+                      </span>
+                    )}
+                    {allocationForPhase && !allocationForPhase.is_public ? (
+                      <Image src='/img/lock.svg' alt='discord' width={15} height={15} />
+                    ) : null}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 };
