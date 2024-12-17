81cc149325aa09f5cc60dc08acbe97e448b10649
0xcodercrane <codercrane@gmail.com>
2024-12-17 23:40:20 +0530

    disable block height

--- src/app/api/blockHeight/route.ts
+++ src/app/api/blockHeight/route.ts
@@ -1,36 +1,14 @@
 import { MEMPOOL_URL, ORDINALSBOT_PUBLIC_API_KEY } from '@/lib/constants';
 import { NextResponse } from 'next/server';
 
-const CACHING_PERIOD_SECONDS = 600;
-
-const fetchBlockHeight = async () => {
-  try {
-    const response = await fetch(`${MEMPOOL_URL}/api/blocks/tip/height`, {
-      headers: {
-        'x-api-key': ORDINALSBOT_PUBLIC_API_KEY
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error(`OrdinalsBot API returned status ${response.status}: ${response.statusText}`);
-    }
-
-    const blockHeight = await response.json();
-
-    if (typeof blockHeight !== 'number') {
-      throw new Error('Invalid response from OrdinalsBot API. Expected a number.');
-    }
-
-    return blockHeight;
-  } catch (e: any) {
-    console.error('Error fetching block height from OrdinalsBot API:', e.message);
-    throw new Error('Fetching block height from OrdinalsBot API failed.');
-  }
-};
-
 export async function GET() {
   try {
-    const blockHeight = await fetchBlockHeight();
+    const blockHeight = await fetch(`${MEMPOOL_URL}/api/blocks/tip/height`, {
+      headers: {
+        'x-api-key': ORDINALSBOT_PUBLIC_API_KEY
+      },
+      cache: 'no-store'
+    });
     const response = NextResponse.json(blockHeight);
 
     response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate');
