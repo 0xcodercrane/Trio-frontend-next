c99d79804a8cb015c3d9a55c8a5520f22a61f45a
0xcodercrane <codercrane@gmail.com>
2024-12-17 23:54:46 +0530

    Revert "disable block height"
    This reverts commit 7f2e8acd538556aea1411fa716c684dac5b9323c.

--- src/app/api/blockHeight/route.ts
+++ src/app/api/blockHeight/route.ts
@@ -1,14 +1,36 @@
 import { MEMPOOL_URL, ORDINALSBOT_PUBLIC_API_KEY } from '@/lib/constants';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+const CACHING_PERIOD_SECONDS = 600;
+
+const fetchBlockHeight = async () => {
   try {
-    const blockHeight = await fetch(`${MEMPOOL_URL}/api/blocks/tip/height`, {
+    const response = await fetch(`${MEMPOOL_URL}/api/blocks/tip/height`, {
       headers: {
         'x-api-key': ORDINALSBOT_PUBLIC_API_KEY
-      },
-      cache: 'no-store'
+      }
     });
+
+    if (!response.ok) {
+      throw new Error(`OrdinalsBot API returned status ${response.status}: ${response.statusText}`);
+    }
+
+    const blockHeight = await response.json();
+
+    if (typeof blockHeight !== 'number') {
+      throw new Error('Invalid response from OrdinalsBot API. Expected a number.');
+    }
+
+    return blockHeight;
+  } catch (e: any) {
+    console.error('Error fetching block height from OrdinalsBot API:', e.message);
+    throw new Error('Fetching block height from OrdinalsBot API failed.');
+  }
+};
+
+export async function GET() {
+  try {
+    const blockHeight = await fetchBlockHeight();
     const response = NextResponse.json(blockHeight);
 
     response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate');
